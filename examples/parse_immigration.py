c946260112eefc43477c325e51d8255c6583cc91
Robin De Schepper <robingilbert.deschepper@unipv.it>
2022-04-13 12:07:17 +0200

    Create parse_immigration.py

---
+++ examples/parse_immigration.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = pd.read_csv("datasets/immigration.csv")
+
+def get_total_df(df):
+    totals = df["AreaName"].str.contains("Total")
+    total_rows = df[totals]
+    years = total_rows.columns[4:-1]
+    immi_data = total_rows[years].to_numpy().astype(float)
+    region_names = total_rows["AreaName"]
+    immi_dict = {region: immi_data[i, :] for i, region in enumerate(region_names)}
+    total_df = pd.DataFrame(immi_dict)
+    total_df["years"] = years
+
+df = get_total_df(data)
